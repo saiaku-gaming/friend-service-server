d3ab8d516c2e298e691e1bc487bc3cbcdc1c8657
Mio Engel <mioengel@gmail.com>
2017-12-22 14:56:15 +0100

    added durable queues

--- src/main/java/com/valhallagame/friendserviceserver/config/FriendConsumer.java
+++ src/main/java/com/valhallagame/friendserviceserver/config/FriendConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FriendConsumer {
-	@RabbitListener(queues = "#{personDeleteQueue.name}")
+	@RabbitListener(queues = "#{friendPersonDeleteQueue.name}")
 	public void receivePersonDelete(String deletedUsername) {
 		System.out.println("WE GOT A MESSAGE: " + deletedUsername);
 	}
