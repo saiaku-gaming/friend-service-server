59ddd1264a5719c280a8cf78f21066fae9736e19
Mikael Berglund <mikbe@kth.se>
2017-12-25 14:13:51 +0100

    More pleasing the linter

--- src/main/java/com/valhallagame/friendserviceserver/controller/FriendController.java
+++ src/main/java/com/valhallagame/friendserviceserver/controller/FriendController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.valhallagame.characterserviceclient.CharacterServiceClient;
 import com.valhallagame.characterserviceclient.message.Character;
 import com.valhallagame.common.JS;
@@ -44,10 +45,14 @@ import com.valhallagame.personserviceclient.model.Person;
 @RequestMapping(path = "/v1/friend")
 public class FriendController {
 
+	private static final String FRIEND_REQUEST_RECEIVED = "Friend request received";
+
+	private static final String COULD_NOT_FIND_PERSON_WITH_USERNAME = "Could not find person with username ";
+
 	private static CharacterServiceClient characterServiceClient = CharacterServiceClient.get();
-	
+
 	private static PersonServiceClient personServiceClient = PersonServiceClient.get();
-	
+
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
@@ -59,12 +64,13 @@ public class FriendController {
 
 	@RequestMapping(path = "/send-person-invite", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> sendPersonInvite(@RequestBody InvitePersonParameter input) throws IOException {
+	public ResponseEntity<JsonNode> sendPersonInvite(@RequestBody InvitePersonParameter input) throws IOException {
 
 		Optional<Person> targetPersonOpt = personServiceClient.getPerson(input.getReceiverUsername()).getResponse();
 
 		if (!targetPersonOpt.isPresent()) {
-			return JS.message(HttpStatus.NOT_FOUND, "Could not find person with username " + input.getReceiverUsername());
+			return JS.message(HttpStatus.NOT_FOUND,
+					"Could not find person with username %s", input.getReceiverUsername());
 		}
 
 		Optional<Invite> optInvite = inviteService.getInviteFromReceiverAndSender(input.getReceiverUsername(),
@@ -90,10 +96,10 @@ public class FriendController {
 			friendService.saveFriend(new Friend(input.getSenderUsername(), input.getReceiverUsername()));
 
 			rabbitTemplate.convertAndSend(RabbitMQRouting.Exchange.FRIEND.name(), RabbitMQRouting.Friend.ADD.name(),
-					new NotificationMessage(input.getSenderUsername(), "Friend request received"));
+					new NotificationMessage(input.getSenderUsername(), FRIEND_REQUEST_RECEIVED));
 
 			rabbitTemplate.convertAndSend(RabbitMQRouting.Exchange.FRIEND.name(), RabbitMQRouting.Friend.ADD.name(),
-					new NotificationMessage(input.getReceiverUsername(), "Friend request received"));
+					new NotificationMessage(input.getReceiverUsername(), FRIEND_REQUEST_RECEIVED));
 
 			return JS.message(HttpStatus.OK,
 					"Friend request accepted since there already was a sent request from the person");
@@ -102,22 +108,26 @@ public class FriendController {
 
 			rabbitTemplate.convertAndSend(RabbitMQRouting.Exchange.FRIEND.name(),
 					RabbitMQRouting.Friend.RECEIVED_INVITE.name(),
-					new NotificationMessage(input.getSenderUsername(), "Friend request received"));
+					new NotificationMessage(input.getSenderUsername(), FRIEND_REQUEST_RECEIVED));
 
 			rabbitTemplate.convertAndSend(RabbitMQRouting.Exchange.FRIEND.name(),
 					RabbitMQRouting.Friend.RECEIVED_INVITE.name(),
-					new NotificationMessage(input.getReceiverUsername(), "Friend request received"));
+					new NotificationMessage(input.getReceiverUsername(), FRIEND_REQUEST_RECEIVED));
 
-			return JS.message(HttpStatus.OK, "Sent a friend request to person with username " + input.getReceiverUsername());
+			return JS.message(HttpStatus.OK,
+					"Sent a friend request to person with username " + input.getReceiverUsername());
 		}
 	}
-	
+
 	@RequestMapping(path = "/send-character-invite", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> sendCharacterInvite(@RequestBody InviteCharacterParameter input) throws IOException {
-		RestResponse<Character> charResp = characterServiceClient.getCharacterWithoutOwnerValidation(input.getReceiverCharacter());
-		if (charResp.isOk()) {
-			Character character = charResp.getResponse().get();
+	public ResponseEntity<JsonNode> sendCharacterInvite(@RequestBody InviteCharacterParameter input)
+			throws IOException {
+		RestResponse<Character> charResp = characterServiceClient
+				.getCharacterWithoutOwnerValidation(input.getReceiverCharacter());
+		Optional<Character> charOpt = charResp.get();
+		if (charOpt.isPresent()) {
+			Character character = charOpt.get();
 			String ownerUsername = character.getOwnerUsername();
 			return sendPersonInvite(new InvitePersonParameter(input.getSenderUsername(), ownerUsername));
 		} else {
@@ -127,10 +137,11 @@ public class FriendController {
 
 	@RequestMapping(path = "/accept-person-invite", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> acceptPersonInvite(@RequestBody AcceptPersonParameter input) throws IOException {
+	public ResponseEntity<JsonNode> acceptPersonInvite(@RequestBody AcceptPersonParameter input) throws IOException {
 
 		if (!personServiceClient.getPerson(input.getAccepteeUsername()).isOk()) {
-			return JS.message(HttpStatus.NOT_FOUND, "Could not find person with username " + input.getAccepteeUsername());
+			return JS.message(HttpStatus.NOT_FOUND,
+					COULD_NOT_FIND_PERSON_WITH_USERNAME + input.getAccepteeUsername());
 		}
 
 		Optional<Invite> optInvite = inviteService.getInviteFromReceiverAndSender(input.getAccepterUsername(),
@@ -153,14 +164,16 @@ public class FriendController {
 
 		return JS.message(HttpStatus.OK, "Friend request accepted");
 	}
-	
-	
+
 	@RequestMapping(path = "/accept-character-invite", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> acceptCharacterInvite(@RequestBody AcceptCharacterParameter input) throws IOException {
-		RestResponse<Character> charResp = characterServiceClient.getCharacterWithoutOwnerValidation(input.getAccepteeCharacterName());
-		if (charResp.isOk()) {
-			Character character = charResp.getResponse().get();
+	public ResponseEntity<JsonNode> acceptCharacterInvite(@RequestBody AcceptCharacterParameter input)
+			throws IOException {
+		RestResponse<Character> charResp = characterServiceClient
+				.getCharacterWithoutOwnerValidation(input.getAccepteeCharacterName());
+		Optional<Character> characterOpt = charResp.get();
+		if (characterOpt.isPresent()) {
+			Character character = characterOpt.get();
 			String ownerUsername = character.getOwnerUsername();
 			return acceptPersonInvite(new AcceptPersonParameter(input.getAccepterUsername(), ownerUsername));
 		} else {
@@ -170,10 +183,10 @@ public class FriendController {
 
 	@RequestMapping(path = "/decline-person-invite", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> declinePersonInvite(@RequestBody DeclinePersonParameter input) throws IOException {
+	public ResponseEntity<JsonNode> declinePersonInvite(@RequestBody DeclinePersonParameter input) throws IOException {
 
 		if (!personServiceClient.getPerson(input.getDeclinee()).isOk()) {
-			return JS.message(HttpStatus.NOT_FOUND, "Could not find person with username " + input.getDeclinee());
+			return JS.message(HttpStatus.NOT_FOUND, COULD_NOT_FIND_PERSON_WITH_USERNAME + input.getDeclinee());
 		}
 
 		Optional<Invite> optInvite = inviteService.getInviteFromReceiverAndSender(input.getDecliner(),
@@ -194,13 +207,16 @@ public class FriendController {
 
 		return JS.message(HttpStatus.OK, "Friend request declined");
 	}
-	
+
 	@RequestMapping(path = "/decline-character-invite", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> declineCharacterInvite(@RequestBody DeclineCharacterParameter input) throws IOException {
-		RestResponse<Character> charResp = characterServiceClient.getCharacterWithoutOwnerValidation(input.getDeclineeCharacterName());
-		if (charResp.isOk()) {
-			Character character = charResp.getResponse().get();
+	public ResponseEntity<JsonNode> declineCharacterInvite(@RequestBody DeclineCharacterParameter input)
+			throws IOException {
+		RestResponse<Character> charResp = characterServiceClient
+				.getCharacterWithoutOwnerValidation(input.getDeclineeCharacterName());
+		Optional<Character> charOpt = charResp.get();
+		if (charOpt.isPresent()) {
+			Character character = charOpt.get();
 			String ownerUsername = character.getOwnerUsername();
 			return declinePersonInvite(new DeclinePersonParameter(input.getDeclinerUsername(), ownerUsername));
 		} else {
@@ -210,10 +226,12 @@ public class FriendController {
 
 	@RequestMapping(path = "/remove-person-friend", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> removePersonFriend(@RequestBody RemovePersonFriendParameter input) throws IOException {
+	public ResponseEntity<JsonNode> removePersonFriend(@RequestBody RemovePersonFriendParameter input)
+			throws IOException {
 
 		if (!personServiceClient.getPerson(input.getRemoveeUsername()).isOk()) {
-			return JS.message(HttpStatus.NOT_FOUND, "Could not find person with username " + input.getRemoveeUsername());
+			return JS.message(HttpStatus.NOT_FOUND,
+					COULD_NOT_FIND_PERSON_WITH_USERNAME + input.getRemoveeUsername());
 		}
 
 		Optional<Friend> optFriend1 = friendService.getFriend(input.getRemoverUsername(), input.getRemoveeUsername());
@@ -237,10 +255,13 @@ public class FriendController {
 
 	@RequestMapping(path = "/remove-character-friend", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> removeCharacterFriend(@RequestBody RemoveCharacterFriendParameter input) throws IOException {
-		RestResponse<Character> charResp = characterServiceClient.getCharacterWithoutOwnerValidation(input.getRemoveeCharacterName());
-		if (charResp.isOk()) {
-			Character character = charResp.getResponse().get();
+	public ResponseEntity<JsonNode> removeCharacterFriend(@RequestBody RemoveCharacterFriendParameter input)
+			throws IOException {
+		RestResponse<Character> charResp = characterServiceClient
+				.getCharacterWithoutOwnerValidation(input.getRemoveeCharacterName());
+		Optional<Character> characterOpt = charResp.get();
+		if (characterOpt.isPresent()) {
+			Character character = characterOpt.get();
 			String ownerUsername = character.getOwnerUsername();
 			return removePersonFriend(new RemovePersonFriendParameter(input.getRemoverUsername(), ownerUsername));
 		} else {
@@ -248,21 +269,21 @@ public class FriendController {
 		}
 
 	}
-	
+
 	@RequestMapping(path = "/get-friend-data", method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<?> getFriendData(@RequestBody UsernameParameter input) throws IOException {
-		
+	public ResponseEntity<JsonNode> getFriendData(@RequestBody UsernameParameter input) throws IOException {
+
 		List<Friend> friends = friendService.getFriends(input.getUsername());
-		
+
 		List<FriendData> friendData = convertToFriendData(friends);
-		
+
 		List<Invite> sentInvites = inviteService.getSentInvites(input.getUsername());
-		
+
 		List<InviteData> sentInvitesData = convertToInviteData(sentInvites);
-		
+
 		List<Invite> receivedInvites = inviteService.getReceivedInvites(input.getUsername());
-		
+
 		List<InviteData> receivedInvitesData = convertToInviteData(receivedInvites);
 
 		return JS.message(HttpStatus.OK, new FriendsData(friendData, sentInvitesData, receivedInvitesData));
@@ -271,7 +292,7 @@ public class FriendController {
 	private List<InviteData> convertToInviteData(List<Invite> sentInvites) throws IOException {
 		List<InviteData> out = new ArrayList<>();
 		for (Invite invite : sentInvites) {
-			convertToInviteData(invite).ifPresent(i -> out.add(i));
+			convertToInviteData(invite).ifPresent(out::add);
 		}
 		return out;
 	}
@@ -279,31 +300,40 @@ public class FriendController {
 	private Optional<InviteData> convertToInviteData(Invite invite) throws IOException {
 		String senderUsername = invite.getSender();
 		String receiverUsername = invite.getReceiver();
-		
+
 		RestResponse<Person> senderPersonResp = personServiceClient.getPerson(senderUsername);
 		RestResponse<Character> senderCharacterResp = characterServiceClient.getSelectedCharacter(senderUsername);
-		
+		Optional<Person> senderPersonOpt = senderPersonResp.get();
+		Optional<Character> senderCharacterOpt = senderCharacterResp.get();
+
 		RestResponse<Person> receiverPersonResp = personServiceClient.getPerson(receiverUsername);
 		RestResponse<Character> receiverCharacterResp = characterServiceClient.getSelectedCharacter(receiverUsername);
-		
-		if(senderPersonResp.isOk() && senderCharacterResp.isOk() && receiverPersonResp.isOk() && receiverCharacterResp.isOk()) {
-			Person senderPerson = senderPersonResp.getResponse().get();
-			Character senderCharacter = senderCharacterResp.getResponse().get();
-			
-			Person receiverPerson = receiverPersonResp.getResponse().get();
-			Character receiverCharacter = receiverCharacterResp.getResponse().get();
-			InviteData inviteData = new InviteData(senderCharacter.getCharacterName(), senderPerson.getDisplayUsername(), receiverCharacter.getCharacterName(), receiverPerson.getDisplayUsername());
+		Optional<Person> receiverPersonOpt = receiverPersonResp.get();
+		Optional<Character> receiverCharacterOpt = receiverCharacterResp.get();
+
+		if (senderPersonOpt.isPresent() && senderCharacterOpt.isPresent() && receiverPersonOpt.isPresent()
+				&& receiverCharacterOpt.isPresent()) {
+			Person senderPerson = senderPersonOpt.get();
+			Character senderCharacter = senderCharacterOpt.get();
+
+			Person receiverPerson = receiverPersonOpt.get();
+			Character receiverCharacter = receiverCharacterOpt.get();
+
+			InviteData inviteData = new InviteData(senderCharacter.getCharacterName(),
+					senderPerson.getDisplayUsername(), receiverCharacter.getCharacterName(),
+					receiverPerson.getDisplayUsername());
+
 			return Optional.of(inviteData);
 		} else {
 			return Optional.empty();
 		}
-		
+
 	}
 
 	private List<FriendData> convertToFriendData(List<Friend> friends) throws IOException {
 		List<FriendData> out = new ArrayList<>();
 		for (Friend friend : friends) {
-			convertToFriendData(friend).ifPresent(f -> out.add(f));
+			convertToFriendData(friend).ifPresent(out::add);
 		}
 		return out;
 	}
@@ -312,11 +342,14 @@ public class FriendController {
 		String friendUsername = friend.getFriend();
 		RestResponse<Person> personResp = personServiceClient.getPerson(friendUsername);
 		RestResponse<Character> characterResp = characterServiceClient.getSelectedCharacter(friendUsername);
+		Optional<Person> personOpt = personResp.get();
+		Optional<Character> characterOpt = characterResp.get();
 		
-		if(personResp.isOk() && characterResp.isOk()) {
-			Person person = personResp.getResponse().get();
-			Character character = characterResp.getResponse().get();
-			return Optional.of(new FriendData(person.isOnline(), person.getDisplayUsername(), character.getDisplayCharacterName()));
+		if (personOpt.isPresent() && characterOpt.isPresent()) {
+			Person person = personOpt.get();
+			Character character = characterOpt.get();
+			return Optional.of(new FriendData(person.isOnline(), person.getDisplayUsername(),
+					character.getDisplayCharacterName()));
 		} else {
 			return Optional.empty();
 		}
