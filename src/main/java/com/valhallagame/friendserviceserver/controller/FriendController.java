d6c6eb966ebea3fd00ac76fd62b2175dd8fe4bfd
Mikael Berglund <mikbe@kth.se>
2017-12-26 12:14:01 +0100

    Consistent model as in:
    Client params are now in message package.
    
    Client responses are in model and ends with Data so that we dont mix it
    up with the database models etc.

--- src/main/java/com/valhallagame/friendserviceserver/controller/FriendController.java
+++ src/main/java/com/valhallagame/friendserviceserver/controller/FriendController.java
@@ -17,29 +17,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.valhallagame.characterserviceclient.CharacterServiceClient;
-import com.valhallagame.characterserviceclient.message.Character;
+import com.valhallagame.characterserviceclient.model.CharacterData;
 import com.valhallagame.common.JS;
 import com.valhallagame.common.RestResponse;
 import com.valhallagame.common.rabbitmq.NotificationMessage;
 import com.valhallagame.common.rabbitmq.RabbitMQRouting;
-import com.valhallagame.friendserviceclient.model.AcceptCharacterParameter;
-import com.valhallagame.friendserviceclient.model.AcceptPersonParameter;
-import com.valhallagame.friendserviceclient.model.DeclineCharacterParameter;
-import com.valhallagame.friendserviceclient.model.DeclinePersonParameter;
+import com.valhallagame.friendserviceclient.message.AcceptCharacterParameter;
+import com.valhallagame.friendserviceclient.message.AcceptPersonParameter;
+import com.valhallagame.friendserviceclient.message.DeclineCharacterParameter;
+import com.valhallagame.friendserviceclient.message.DeclinePersonParameter;
+import com.valhallagame.friendserviceclient.message.InviteCharacterParameter;
+import com.valhallagame.friendserviceclient.message.InvitePersonParameter;
+import com.valhallagame.friendserviceclient.message.RemoveCharacterFriendParameter;
+import com.valhallagame.friendserviceclient.message.RemovePersonFriendParameter;
+import com.valhallagame.friendserviceclient.message.UsernameParameter;
 import com.valhallagame.friendserviceclient.model.FriendData;
 import com.valhallagame.friendserviceclient.model.FriendsData;
-import com.valhallagame.friendserviceclient.model.InviteCharacterParameter;
 import com.valhallagame.friendserviceclient.model.InviteData;
-import com.valhallagame.friendserviceclient.model.InvitePersonParameter;
-import com.valhallagame.friendserviceclient.model.RemoveCharacterFriendParameter;
-import com.valhallagame.friendserviceclient.model.RemovePersonFriendParameter;
-import com.valhallagame.friendserviceclient.model.UsernameParameter;
 import com.valhallagame.friendserviceserver.model.Friend;
 import com.valhallagame.friendserviceserver.model.Invite;
 import com.valhallagame.friendserviceserver.service.FriendService;
 import com.valhallagame.friendserviceserver.service.InviteService;
 import com.valhallagame.personserviceclient.PersonServiceClient;
-import com.valhallagame.personserviceclient.model.Person;
+import com.valhallagame.personserviceclient.model.PersonData;
 
 @Controller
 @RequestMapping(path = "/v1/friend")
@@ -66,7 +66,7 @@ public class FriendController {
 	@ResponseBody
 	public ResponseEntity<JsonNode> sendPersonInvite(@RequestBody InvitePersonParameter input) throws IOException {
 
-		Optional<Person> targetPersonOpt = personServiceClient.getPerson(input.getReceiverUsername()).getResponse();
+		Optional<PersonData> targetPersonOpt = personServiceClient.getPerson(input.getReceiverUsername()).getResponse();
 
 		if (!targetPersonOpt.isPresent()) {
 			return JS.message(HttpStatus.NOT_FOUND,
@@ -123,11 +123,11 @@ public class FriendController {
 	@ResponseBody
 	public ResponseEntity<JsonNode> sendCharacterInvite(@RequestBody InviteCharacterParameter input)
 			throws IOException {
-		RestResponse<Character> charResp = characterServiceClient
+		RestResponse<CharacterData> charResp = characterServiceClient
 				.getCharacterWithoutOwnerValidation(input.getReceiverCharacter());
-		Optional<Character> charOpt = charResp.get();
+		Optional<CharacterData> charOpt = charResp.get();
 		if (charOpt.isPresent()) {
-			Character character = charOpt.get();
+			CharacterData character = charOpt.get();
 			String ownerUsername = character.getOwnerUsername();
 			return sendPersonInvite(new InvitePersonParameter(input.getSenderUsername(), ownerUsername));
 		} else {
@@ -169,11 +169,11 @@ public class FriendController {
 	@ResponseBody
 	public ResponseEntity<JsonNode> acceptCharacterInvite(@RequestBody AcceptCharacterParameter input)
 			throws IOException {
-		RestResponse<Character> charResp = characterServiceClient
+		RestResponse<CharacterData> charResp = characterServiceClient
 				.getCharacterWithoutOwnerValidation(input.getAccepteeCharacterName());
-		Optional<Character> characterOpt = charResp.get();
+		Optional<CharacterData> characterOpt = charResp.get();
 		if (characterOpt.isPresent()) {
-			Character character = characterOpt.get();
+			CharacterData character = characterOpt.get();
 			String ownerUsername = character.getOwnerUsername();
 			return acceptPersonInvite(new AcceptPersonParameter(input.getAccepterUsername(), ownerUsername));
 		} else {
@@ -212,11 +212,11 @@ public class FriendController {
 	@ResponseBody
 	public ResponseEntity<JsonNode> declineCharacterInvite(@RequestBody DeclineCharacterParameter input)
 			throws IOException {
-		RestResponse<Character> charResp = characterServiceClient
+		RestResponse<CharacterData> charResp = characterServiceClient
 				.getCharacterWithoutOwnerValidation(input.getDeclineeCharacterName());
-		Optional<Character> charOpt = charResp.get();
+		Optional<CharacterData> charOpt = charResp.get();
 		if (charOpt.isPresent()) {
-			Character character = charOpt.get();
+			CharacterData character = charOpt.get();
 			String ownerUsername = character.getOwnerUsername();
 			return declinePersonInvite(new DeclinePersonParameter(input.getDeclinerUsername(), ownerUsername));
 		} else {
@@ -257,11 +257,11 @@ public class FriendController {
 	@ResponseBody
 	public ResponseEntity<JsonNode> removeCharacterFriend(@RequestBody RemoveCharacterFriendParameter input)
 			throws IOException {
-		RestResponse<Character> charResp = characterServiceClient
+		RestResponse<CharacterData> charResp = characterServiceClient
 				.getCharacterWithoutOwnerValidation(input.getRemoveeCharacterName());
-		Optional<Character> characterOpt = charResp.get();
+		Optional<CharacterData> characterOpt = charResp.get();
 		if (characterOpt.isPresent()) {
-			Character character = characterOpt.get();
+			CharacterData character = characterOpt.get();
 			String ownerUsername = character.getOwnerUsername();
 			return removePersonFriend(new RemovePersonFriendParameter(input.getRemoverUsername(), ownerUsername));
 		} else {
@@ -301,23 +301,23 @@ public class FriendController {
 		String senderUsername = invite.getSender();
 		String receiverUsername = invite.getReceiver();
 
-		RestResponse<Person> senderPersonResp = personServiceClient.getPerson(senderUsername);
-		RestResponse<Character> senderCharacterResp = characterServiceClient.getSelectedCharacter(senderUsername);
-		Optional<Person> senderPersonOpt = senderPersonResp.get();
-		Optional<Character> senderCharacterOpt = senderCharacterResp.get();
+		RestResponse<PersonData> senderPersonResp = personServiceClient.getPerson(senderUsername);
+		RestResponse<CharacterData> senderCharacterResp = characterServiceClient.getSelectedCharacter(senderUsername);
+		Optional<PersonData> senderPersonOpt = senderPersonResp.get();
+		Optional<CharacterData> senderCharacterOpt = senderCharacterResp.get();
 
-		RestResponse<Person> receiverPersonResp = personServiceClient.getPerson(receiverUsername);
-		RestResponse<Character> receiverCharacterResp = characterServiceClient.getSelectedCharacter(receiverUsername);
-		Optional<Person> receiverPersonOpt = receiverPersonResp.get();
-		Optional<Character> receiverCharacterOpt = receiverCharacterResp.get();
+		RestResponse<PersonData> receiverPersonResp = personServiceClient.getPerson(receiverUsername);
+		RestResponse<CharacterData> receiverCharacterResp = characterServiceClient.getSelectedCharacter(receiverUsername);
+		Optional<PersonData> receiverPersonOpt = receiverPersonResp.get();
+		Optional<CharacterData> receiverCharacterOpt = receiverCharacterResp.get();
 
 		if (senderPersonOpt.isPresent() && senderCharacterOpt.isPresent() && receiverPersonOpt.isPresent()
 				&& receiverCharacterOpt.isPresent()) {
-			Person senderPerson = senderPersonOpt.get();
-			Character senderCharacter = senderCharacterOpt.get();
+			PersonData senderPerson = senderPersonOpt.get();
+			CharacterData senderCharacter = senderCharacterOpt.get();
 
-			Person receiverPerson = receiverPersonOpt.get();
-			Character receiverCharacter = receiverCharacterOpt.get();
+			PersonData receiverPerson = receiverPersonOpt.get();
+			CharacterData receiverCharacter = receiverCharacterOpt.get();
 
 			InviteData inviteData = new InviteData(senderCharacter.getCharacterName(),
 					senderPerson.getDisplayUsername(), receiverCharacter.getCharacterName(),
@@ -340,14 +340,14 @@ public class FriendController {
 
 	private Optional<FriendData> convertToFriendData(Friend friend) throws IOException {
 		String friendUsername = friend.getFriendUsername();
-		RestResponse<Person> personResp = personServiceClient.getPerson(friendUsername);
-		RestResponse<Character> characterResp = characterServiceClient.getSelectedCharacter(friendUsername);
-		Optional<Person> personOpt = personResp.get();
-		Optional<Character> characterOpt = characterResp.get();
+		RestResponse<PersonData> personResp = personServiceClient.getPerson(friendUsername);
+		RestResponse<CharacterData> characterResp = characterServiceClient.getSelectedCharacter(friendUsername);
+		Optional<PersonData> personOpt = personResp.get();
+		Optional<CharacterData> characterOpt = characterResp.get();
 		
 		if (personOpt.isPresent() && characterOpt.isPresent()) {
-			Person person = personOpt.get();
-			Character character = characterOpt.get();
+			PersonData person = personOpt.get();
+			CharacterData character = characterOpt.get();
 			return Optional.of(new FriendData(person.isOnline(), person.getDisplayUsername(),
 					character.getDisplayCharacterName()));
 		} else {
